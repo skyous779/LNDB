746aa82587e5951b68b2eaa91c001a6b03eeeaad
skyous779 <1019364238@qq.com>
2021-07-15 14:46:00 +0800

    爆内存

---
+++ WeightedSampler_test.py
@@ -0,0 +1,30 @@
+from glob import glob
+from pathlib import Path
+from hparam import hparams as hp
+import torchio as tio
+
+images_dir = hp.source_train_dir
+labels_dir = hp.label_train_dir
+
+images_dir = Path(images_dir)
+image_paths = sorted(images_dir.glob(hp.fold_arch))
+
+labels_dir = Path(labels_dir)
+label_paths = sorted(labels_dir.glob(hp.fold_arch))   #进行遍历排序
+subjects = []
+patch_size = 32
+sampler = tio.data.WeightedSampler(patch_size, 'sampling_map')
+for (image_path, label_path) in zip(image_paths, label_paths):
+    subject = tio.Subject(
+        source=tio.ScalarImage(image_path),
+        sampling_map=tio.Image(label_path,type=tio.SAMPLING_MAP),
+    )
+    transform = tio.CropOrPad(
+        (512,512,512),
+        mask_name="sampling_map",    #爆内存
+    )
+    
+    subject = transform(subject)
+    for patch in sampler(subject,num_patches=1):
+        print(patch[tio.LOCATION])
+    subjects.append(subject)    
